1a324560657b9e91e34fd3b064dc871304c25e7e
lrvaka <lukevakasisikakala@gmail.com>
2023-08-21 19:25:13 -0400

    add navbar, navbar animation, header animation

--- components/Home/Grid/GridItems/GridItem/HeaderGridItem.tsx
+++ components/Home/Grid/GridItems/GridItem/HeaderGridItem.tsx
@@ -6,27 +6,44 @@ import headshot3 from "@/public/headshot-3.png";
 import { AiOutlineReload } from "react-icons/ai";
 import { useState } from "react";
 
-const HeaderGridItem = () => {
-  const headshots = [headshot, headshot1, headshot2, headshot3];
+const HEADSHOTS = [headshot, headshot1, headshot2, headshot3];
 
+const HeaderGridItem = () => {
   const [headshotIndex, setHeadshotIndex] = useState(0);
   const [opacity, setOpacity] = useState(1);
+  const [rotation, setRotation] = useState(0);
+  const [scale, setScale] = useState(1);
+
+  const [blobOpacity, setBlobOpacity] = useState(0);
+  const [blobScale, setBlobScale] = useState(0);
 
   const handleHeadshotChange = () => {
     // Start fade out animation
     setOpacity(0);
+    setRotation(rotation + 45);
+    setScale(0.5);
+
+    // Start blob animation
+    setBlobOpacity(1);
+    setBlobScale(0);
 
     // Wait for the fade out animation to complete
     setTimeout(() => {
       let randomIndex;
       do {
-        randomIndex = Math.floor(Math.random() * headshots.length);
+        randomIndex = Math.floor(Math.random() * HEADSHOTS.length);
       } while (randomIndex === headshotIndex);
 
       setHeadshotIndex(randomIndex);
 
       // Fade in animation
       setOpacity(1);
+      setRotation(0);
+      setScale(1);
+
+      // Blob animation
+      setBlobOpacity(0);
+      setBlobScale(2);
     }, 300); // 300ms is the duration of the fade effect
   };
 
@@ -36,22 +53,37 @@ const HeaderGridItem = () => {
         onClick={handleHeadshotChange}
         className="p-2 rounded-full shadow-3xl hover:shadow-4xl transition-all absolute right-4 top-4 bg-white"
       >
-        <AiOutlineReload />
-      </button>
-      <div className="flex justify-between">
-        <div className=" w-28 h-28 relative">
-          <Image
-            src={headshots[headshotIndex]}
-            alt="hansom boy"
-            fill
-            style={{
-              objectFit: "contain",
-              transition: "opacity 0.3s",
-              opacity: opacity,
-            }}
-          />
+        <div style={{ rotate: `${rotation * 5}deg`, transition: "all 0.3s" }}>
+          <AiOutlineReload />
         </div>
+      </button>
+
+      <div className=" w-28 h-28 relative">
+        <Image
+          src={HEADSHOTS[headshotIndex]}
+          alt="hansom boy"
+          fill
+          style={{
+            objectFit: "contain",
+            transition: "all 0.3s",
+            opacity: opacity,
+            rotate: `${rotation}deg`,
+            scale: `${scale}`,
+          }}
+        />
+        <div
+          style={{
+            transition: "all 0.5s",
+            opacity: blobOpacity,
+            scale: `${blobScale}`,
+          }}
+          onTransitionEnd={() => {
+            setBlobScale(0);
+          }}
+          className="relative scale-150 -z-10 blur-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 w-full h-full"
+        ></div>
       </div>
+
       <h2>
         I&apos;m <span className=" font-bold text-xl">Luke</span>, a developer
         and designer based in NYC. I&apos;m interested in React, Jamstack,
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,52 +1,76 @@
 import Image from "next/image";
-import headshot from "../public/headshot.png";
+import logo from "@/public/logo.png";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const navItems = [
+  {
+    name: "All",
+    href: "index.html",
+  },
+  {
+    name: "About",
+    href: "about.html",
+  },
+  {
+    name: "Projects",
+    href: "contact.html",
+  },
+  {
+    name: "Media",
+    href: "contact.html",
+  },
+];
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+  const [blobWidth, setBlobWidth] = useState(49.4297);
+  const [blobLeft, setBlobLeft] = useState(5);
+
+  const handleNavItemClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLElement;
+    setBlobWidth(target.getBoundingClientRect().width);
+    setBlobLeft(target.offsetLeft);
+    setSelectedItemIndex(parseInt(target.getAttribute("data-index")!));
+  };
+
   return (
     <div className={inter.className}>
-      <div className="fixed inset-x-0 bottom-0 flex justify-center py-10 ">
-        <nav className="bg-black rounded-md p-5">
-          <ul className="flex gap-10 ">
-            <li className="text-white p-2  ">
-              <a href="index.html">Home</a>
-            </li>
-            <li className="text-white p-2  ">
-              <a href="about.html">About</a>
-            </li>
-            <li className="text-white p-2 ">
-              <a href="contact.html">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <div className="flex justify-between items-center gap-5 pt-10 max-w-7xl px-4 mx-auto">
+        <Image src={logo} alt="cool name" width={100} height={20} />
 
-      <div className="px-4 max-w-6xl mx-auto">
-        <div className="flex justify-between items-center gap-5 pt-20">
-          <div className="flex items-center gap-5 ">
-            <Image
-              style={{ width: "100px" }}
-              src={headshot}
-              alt="hansome boy"
-            />
-            <h1 className="font-bold text-3xl">
-              Luke Vakasisikakala <span className="sr-only">—</span> <br />
-              <span className=" font-normal text-xl ">
-                Front-End Developer at Blockhead Digital
-              </span>
-            </h1>
-          </div>
-          <div className="grid grid-cols-2 gap-2 text-white">
-            <div className="bg-slate-700 p-1">github</div>
-            <div className="bg-slate-700 p-1">LinkedIn</div>
-            <div className="bg-slate-700 p-1">Twitter</div>
-            <div className="bg-slate-700 p-1">Instagram</div>
-          </div>
-        </div>
-        {children}
+        <header>
+          <nav>
+            <ul className="flex bg-gray-200 p-[5px] rounded-full relative">
+              <div
+                className="w-12 h-8 absolute bg-white rounded-2xl"
+                style={{
+                  left: `${blobLeft}px`,
+                  width: `${blobWidth}px`,
+                  transition: "all 0.3s",
+                }}
+              ></div>
+              {navItems.map((item, index) => (
+                <div
+                  className="font-semibold text-sm cursor-pointer z-10 px-4 h-8 flex items-center"
+                  onClick={handleNavItemClick}
+                  key={index}
+                  data-index={index}
+                >
+                  {item.name}
+                </div>
+              ))}
+            </ul>
+          </nav>
+        </header>
+
+        <a>
+          <p className="font-semibold text-sm">Contact</p>
+        </a>
       </div>
+      <div className="px-4 max-w-6xl mx-auto">{children}</div>
     </div>
   );
 };
